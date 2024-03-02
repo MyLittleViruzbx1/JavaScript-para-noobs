4c1b6d74d6c00bd53a5e6f1654f3f47514303c78
Myz <mylittleviruz@outlook.es>
2024-03-02 00:44:07 -0600

    mi primer comit de mi destruccion como programador

---
+++ Curso JS Moderno/22-Prototypes/js/01-app.js
@@ -0,0 +1,18 @@
+const cliente = {
+    nombre: 'willy',
+    saldo: 500
+}
+
+console.log(cliente);
+console.log(typeof cliente);
+
+
+function Cliente (nombre, saldo){
+    this.nombre = nombre;
+    this.saldo = saldo;
+}
+
+
+const juan = new Cliente('juan', 500);
+
+console.log(juan)
